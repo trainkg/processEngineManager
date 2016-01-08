d4bcc6caf235bce87c97f4f96f73bba8d4f50dec
朱元宇 <peculiar.1@163.com>
2016-01-09 00:11:07 +0800

    layout

--- spider-war/src/main/webapp/statics/processManagerApplication.js
+++ spider-war/src/main/webapp/statics/processManagerApplication.js
@@ -4,7 +4,7 @@
 require([
          'backbone',
          'underscore',
-         'core/panel'
+         'core/layout'
      ],function(Backbone,_,Window){
 	var controller  = Backbone.Router.extend({
 		initialize:function(){
@@ -19,21 +19,14 @@ require([
 			
 			var panel1 =  new Window();
 			var panel2 =  new Window({
-					resize : false,
-					type : 'border', // border | flow | flexgrid | grid
-					vgap : 4,		 
-					hgap : 4,		 
-					westWt:20,
-					southHt:50,
-					eastWt:20,
-					showNorth:false,
-					showWest:false
+				southHt:50,
+				eastWt:100,
+				showNorth:false,
+				showWest:false
 			});
 			panel.render();
-			panel.addBorderComponent('center',panel1);
-			panel.addBorderComponent('east',panel2);
-//			panel.addBorderComponent('south',panel3);
-//			panel.addBorderComponent('west',panel4);
+			panel.addBorderComponent('center',panel2);
+//			panel.addBorderComponent('east',panel2);
 		}
 	});
 	
